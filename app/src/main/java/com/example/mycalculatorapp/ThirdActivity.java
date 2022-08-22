c15f6449518837d46fc177cf300c5799e4777828
sophizhao <90772792+sophizhao@users.noreply.github.com>
2022-08-22 08:20:11 -0500

    Decided not to do sigfig

--- app/src/main/java/com/example/mycalculatorapp/ThirdActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/ThirdActivity.java
@@ -2,12 +2,23 @@ package com.example.mycalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    public void convertPounds(){
     }
 }
